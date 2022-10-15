ffcfd90956dfd17659403d5e4ad041b428d5b0ed
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-15 19:15:44 +0200

    header html done

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -16,7 +19,13 @@ function App() {
       <ToastContainer position="bottom-center" limit={1} />
       <div className="App">
         <Header />
-        <div className="main"></div>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,42 @@
 import React from "react";
-import logoImg from "../assets/logo-commerce.png";
+import logoImg from "../assets/logo-main-cr.png";
 import { AiOutlineSearch } from "react-icons/ai";
+import { GrLocation } from "react-icons/gr";
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header">
       <div className="header_up">
         <div className="logo">
-          <img className="logo_img" src={logoImg} alt="logo" />
+          <Link to="/" className="link_class">
+            <img className="logo_img" src={logoImg} alt="logo" />
+          </Link>
         </div>
         <div className="adress">
-          <AiOutlineSearch />
+          <GrLocation className="gr_icon" />
           <div className="adress_text">
-            <p>Hello</p>
-            <p>Select your address</p>
+            <p className="hello1">Hello</p>
+            <p className="hello2">Select your address</p>
           </div>
         </div>
-        <div className="adress"></div>
-        <div className="adress"></div>
-        <div className="adress"></div>
+        <div className="search-box">
+          <div className="search-contents">
+            <input type="text" />
+            <div className="search-contents-icon">
+              <AiOutlineSearch />
+            </div>
+          </div>
+        </div>
+        <div className="shop-card">
+          <FiShoppingCart />
+          <span>2</span>
+        </div>
+        <div className="auth-box">
+          <Link to="/login" className="link_class">
+            Sign In
+          </Link>
+        </div>
       </div>
       <div className="header_down"></div>
     </div>
