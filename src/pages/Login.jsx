e8b647d294049820b71eb3d0ab4da6e1dc9eb482
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-15 22:13:00 +0200

    Login along with func done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,105 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import logoImg from "../assets/logo-main.png";
+import { AiOutlineGoogle } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+
 const Login = () => {
+  const [loggedUser, setLoggedUser] = useState({ email: "", password: "" });
+  const { email, password } = loggedUser;
+  const [passType, setPassType] = useState(false);
+
+  const handleInput = (e) => {
+    setLoggedUser({ ...loggedUser, [e.target.name]: e.target.value });
+  };
+
+  console.log(loggedUser);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/v1/auth/login", {
+        ...loggedUser,
+      });
+      console.log(data);
+      toast.success("You are in");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  const handleCancel = () => {
+    setLoggedUser({ ...loggedUser, email: "", password: "" });
+  };
+
   return (
     <div className="login">
       <div className="login_sorround">
         <img src={logoImg} alt="" className="login_img" />
-        <form className="login_form">
-          <input type="email" placeholder="Type your email" />
-          <input type="password" placeholder="Type your password" />
-          <button className="btn btn-primary">Sign In</button>
-          <button className="btn btn-danger">Cancel</button>
+        <form onSubmit={handleSubmit} className="login_form">
+          <h2>Sign in</h2>
+          <input
+            required
+            name="email"
+            type="email"
+            placeholder="Type your email"
+            value={email}
+            onChange={handleInput}
+          />
+          <div className="pass_box">
+            <input
+              name="password"
+              required
+              type={passType ? "text" : "password"}
+              placeholder="Type your password"
+              value={password}
+              onChange={handleInput}
+            />
+            <small
+              style={{ color: passType ? "red" : "teal" }}
+              onClick={() => setPassType(!passType)}
+            >
+              {passType ? "Hide" : "Show"}
+            </small>
+          </div>
+          <div className="login_form_btns">
+            <button type="submit" className="btn btn-primary">
+              Sign In
+            </button>
+            <button
+              onClick={handleCancel}
+              type="button"
+              className="btn btn-danger"
+            >
+              Cancel
+            </button>
+          </div>
+          <div className="oauth_login_btns">
+            <button className="btn google_btn">
+              <AiOutlineGoogle size={20} /> Login With Google
+            </button>
+            <button className="btn facebbok_btn">
+              <FaFacebookF size={20} />
+              Login With Facebook
+            </button>
+          </div>
+          <div className="forgot_password">
+            <p className="forgot">Forgot Password?</p>
+          </div>
         </form>
         <div className="forward_register">
           <div className="forward_register_line">
-            <div className="forward_left"></div>
+            <div className="forward_line"></div>
             <div className="forward_text">New to e-Commerce</div>
-            <div className="forward_right"></div>
+            <div className="forward_line"></div>
           </div>
-          <button className="btn">Create an Account</button>
+          <Link to="/register" className="link_class">
+            <button className="btn btn-secondary btn-create">
+              Create an Account
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import logoImg from "../assets/logo-main.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [signUpUser, setSignUpUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    imageId: "pw4gq42vstslcyqzi81o",
+    roles: ["User"],
+  });
+  const { firstName, lastName, imageId, email, password } = signUpUser;
+  const [passType, setPassType] = useState(false);
+
+  const handleInput = (e) => {
+    setSignUpUser({ ...signUpUser, [e.target.name]: e.target.value });
+  };
+
+  console.log(signUpUser);
+
+  const handleSubmit = async (e) => {};
+
+  const handleCancel = () => {
+    setSignUpUser({ ...signUpUser, email: "", password: "" });
+  };
+
+  return (
+    <div className="register">
+      <div className="register_sorround">
+        <img src={logoImg} alt="" className="register_img" />
+        <form onSubmit={handleSubmit} className="register_form">
+          <h2>Sign up</h2>
+          <input
+            required
+            name="email"
+            type="email"
+            placeholder="Type your email"
+            value={email}
+            onChange={handleInput}
+          />
+          <div className="pass_box">
+            <input
+              name="password"
+              required
+              type={passType ? "text" : "password"}
+              placeholder="Type your password"
+              value={password}
+              onChange={handleInput}
+            />
+            <small
+              style={{ color: passType ? "red" : "teal" }}
+              onClick={() => setPassType(!passType)}
+            >
+              {passType ? "Hide" : "Show"}
+            </small>
+          </div>
+          <div className="register_form_btns">
+            <button type="submit" className="btn btn-primary">
+              Sign In
+            </button>
+            <button
+              onClick={handleCancel}
+              type="button"
+              className="btn btn-danger"
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+        <div className="forward_register">
+          <div className="forward_register_line">
+            <div className="forward_line"></div>
+            <div className="forward_text">Already have e-Commerce account</div>
+            <div className="forward_line"></div>
+          </div>
+          <Link to="/login" className="link_class">
+            <button className="btn btn-primary btn-create">Sign In</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
