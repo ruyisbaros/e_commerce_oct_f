38fd36fa42a0b7d513827978c79d44bb5e16a408
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-24 00:35:33 +0200

    proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+import proxy from "http-proxy-middleware";
+module.exports = function (app) {
+  app.use(
+    proxy("https://my-ecom-back.herokuapp.com", {
+      target: "http://localhost:8090/",
+      /* pathRewrite: {
+        '^/\\.netlify/functions': '',
+      }, */
+    })
+  );
+};
