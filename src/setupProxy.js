a202dc939eaea116d7bb62840a8920ca67970564
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-10-23 15:17:17 +0200

    deploy failure

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(["/api", , "/otherApi"], {
+      target: "http://localhost:8090",
+    })
+  );
+};
